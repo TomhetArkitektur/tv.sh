9e023c9fda6095d3b5ac4548db168a2fd21bd43e
alsvartr <nazarov.pn@gmail.com>
2026-02-15 22:36:46 +0300

    move sources

---
+++ lib/sources.sh
@@ -0,0 +1,46 @@
+get_file_tvsh() {
+  url="$1"
+
+  fname=$(basename "$url")
+  curl -s -o "$CACHE_DIR/$fname" "$url"
+  echo "$CACHE_DIR/$fname"
+}
+
+get_file_immich() {
+  url="$1"
+
+  fname=$(basename $(dirname "$url"))
+  curl -s -o "$CACHE_DIR/$fname" -H "Accept: application/json" -H "x-api-key: $IMMICH_API_KEY" -L "$url"
+  echo "$CACHE_DIR/$fname"
+}
+
+get_random_tvsh() {
+  dura="$1"
+
+  uri="$TVSH_URI/?random"
+  if [ "$dura" != "" ]; then
+    uri="$uri&duration=$dura"
+  fi
+  json=$(curl -s "$uri" | head -1)
+
+  url=$(echo "$json" | jq -r '.url')
+  fdura=$(echo "$json" | jq -r '.duration')
+  name=$(basename "$url")
+  echo "$name" "$url" "$fdura" ""
+}
+
+get_random_immich() {
+  rand_line=$(shuf -n 1 "$TMP_DIR/immich_assets")
+  asset=`echo "$rand_line" | awk -F';' '{print $1}'`
+  city=`echo "$rand_line" | awk -F';' '{print $2}'`
+  state=`echo "$rand_line" | awk -F';' '{print $3}'`
+  if [ "$city" == "null" ]; then
+    city=""
+  fi
+  if [[ "$state" == "null" || "$state" == "$city" ]]; then
+    state=""
+  fi
+  location="$city $state"
+
+  echo "$asset" "$IMMICH_URL/assets/$asset/original" 0 "$location"
+}
--- play.sh
+++ play.sh
@@ -3,42 +3,13 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$DIR/lib/config.sh"
 source "$DIR/lib/utils.sh"
+source "$DIR/lib/sources.sh"
 
 show_osd() {
   if [ "$SHOW_OSD" == "true" ]; then
     text="$1"
     echo '{"command": ["show-text", "'"$text"'", "'"$OSD_TIME"'"]}' | socat - "$MPV_SOCK" >/dev/null
   fi
-}
-
-get_random_tvsh() {
-  dura="$1"
-
-  uri="$TVSH_URI/?random"
-  if [ "$dura" != "" ]; then
-    uri="$uri&duration=$dura"
-  fi
-  json=$(curl -s "$uri" | head -1)
-
-  url=$(echo "$json" | jq -r '.url')
-  fdura=$(echo "$json" | jq -r '.duration')
-  echo "$url" "$fdura" ""
-}
-
-get_random_immich() {
-  rand_line=$(shuf -n 1 "$TMP_DIR/immich_assets")
-  asset=`echo "$rand_line" | awk -F';' '{print $1}'`
-  city=`echo "$rand_line" | awk -F';' '{print $2}'`
-  state=`echo "$rand_line" | awk -F';' '{print $3}'`
-  if [ "$city" == "null" ]; then
-    city=""
-  fi
-  if [[ "$state" == "null" || "$state" == "$city" ]]; then
-    state=""
-  fi
-  location="$city $state"
-
-  echo "$asset" "$IMMICH_URL/assets/$asset/original" 0 "$location"
 }
 
 get_random() {
@@ -50,22 +21,6 @@ get_random() {
     read -r name url fdura osd < <(get_random_immich)
   fi
   echo "$name" "$url" "$fdura" "$osd"
-}
-
-get_file_tvsh() {
-  url="$1"
-
-  fname=$(basename "$url")
-  curl -s -o "$CACHE_DIR/$fname" "$url"
-  echo "$CACHE_DIR/$fname"
-}
-
-get_file_immich() {
-  url="$1"
-
-  fname=$(basename $(dirname "$url"))
-  curl -s -o "$CACHE_DIR/$fname" -H "Accept: application/json" -H "x-api-key: $IMMICH_API_KEY" -L "$url"
-  echo "$CACHE_DIR/$fname"
 }
 
 get_file() {
